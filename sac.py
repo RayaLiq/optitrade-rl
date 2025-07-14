ebe080d96a25e1330edbed78fab65532a4ce7c1f
Nima <n9e6y@outlook.com>
2025-07-14 19:40:24 +0330

    Remove sd3_agents and split TD3 and SAC into separate files

---
+++ sac.py
@@ -0,0 +1,23 @@
+import numpy as np
+from stable_baselines3 import SAC
+from stable_baselines3.common.env_util import DummyVecEnv
+
+class SB3SACAgent:
+    def __init__(self, env, policy_kwargs=None, **kwargs):
+        self.env = DummyVecEnv([lambda: env])
+        self.model = SAC('MlpPolicy', self.env, policy_kwargs=policy_kwargs, **kwargs)
+        self.last_obs = None
+
+    def act(self, state, add_noise=True):
+        action, _ = self.model.predict(state, deterministic=not add_noise)
+        return action
+
+    def step(self, state, action, reward, next_state, done):
+        # SB3 handles training internally; this is a no-op for compatibility
+        pass
+
+    def reset(self):
+        self.last_obs = None
+
+    def learn(self, total_timesteps):
+        self.model.learn(total_timesteps=total_timesteps) 

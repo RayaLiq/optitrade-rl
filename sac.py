61b050c39c91a4bcb39f2c0b7299eae385409103
Nima <n9e6y@outlook.com>
2025-07-16 16:38:24 +0330

    SAC added

--- sac.py
+++ sac.py
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import numpy as np
-from stable_baselines3 import SAC
-from stable_baselines3.common.env_util import DummyVecEnv
-
-class SB3SACAgent:
-    def __init__(self, env, policy_kwargs=None, **kwargs):
-        self.env = DummyVecEnv([lambda: env])
-        self.model = SAC('MlpPolicy', self.env, policy_kwargs=policy_kwargs, **kwargs)
-        self.last_obs = None
-
-    def act(self, state, add_noise=True):
-        action, _ = self.model.predict(state, deterministic=not add_noise)
-        return action
-
-    def step(self, state, action, reward, next_state, done):
-        # SB3 handles training internally; this is a no-op for compatibility
-        pass
-
-    def reset(self):
-        self.last_obs = None
-
-    def learn(self, total_timesteps):
-        self.model.learn(total_timesteps=total_timesteps) 
-=======
-# sac.py
 import numpy as np
 from stable_baselines3 import SAC
 from stable_baselines3.common.env_util import make_vec_env
@@ -93,6 +67,4 @@ class SB3SACAgent:
         A dummy 'reset' method for compatibility. SAC's internal state (if any)
         is managed within the `.learn()` method.
         """
-        pass  
-
->>>>>>> d3b7565988697e0fc23ab8cbbb17e965ad28ecb8
+        pass 
